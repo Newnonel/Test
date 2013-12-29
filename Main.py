836759f0b6ad809e7f69cde72462a89ccf89f74a
Newnonel <olive_tree132@hotmail.com>
2013-12-29 22:36:32 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,14 +5,44 @@
 #Create general program loop
 #While you choose to live, you live.
 import time
+import random
 Gameover = False;
-
+print("You face the Dragon. How long can you live?")
+Dragon_HP=10
+Dragon_Attack=0
+Your_life=0
+Player_Attack=0
 while not Gameover:
-    Player_Input = input("Press 'e' to exit or 'l' to live: "); #Gather user Input
+    Player_Input = input("Press 'd' to dodge or 'r' to run or 's' to strike: "); #Gather user Input
+    Dragon_Attack = random.randint(1,10)
+    Player_Attack = random.randint(1,10)+Your_life
     
     #Begin processing user input
-    if Player_Input == 'e':
-        Gameover = True;
-    elif Player_Input =='l':
-        print("You have choosen to live indeed. A wise choice.")
-        time.sleep(5)
+    if Dragon_Hp<0:
+        break
+    else:
+        if Player_Input == 'r':
+            Gameover = True;
+            print("You lived for %s seconds. You did not slay the Dragon.")%Your_life
+            time.sleep(5)
+        elif Player_Input =='s':
+            print("You lunge at the Dragon!")
+            time.sleep(5)
+            if Dragon_Attack>Player_Attack:
+                print("The Dragon snaps and swallows you whole!")
+                print("You lived for %s seconds. You did not slay the Dragon.")%Your_life
+            else:
+                print("You cut the Dragon!")
+                Your_life+=1
+                Dragon_Hp-=1
+        else Player_Input=='d':
+            print("You try to dodge the Dragon's attack!")
+            Player_Attack=Player_Attack+2
+            if Dragon_Attack>Player_Attack:
+                print("The Dragon snaps and swallows you whole!")
+                print("You lived for %s seconds. You did not slay the Dragon.")%Your_life
+                time.sleep(5)
+        else:
+            print("You dodged the Dragon!")
+print("You defeated the Dragon!")
+time.sleep(5)
