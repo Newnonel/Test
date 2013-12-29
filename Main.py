7bdb33ccc834928fb85b10adddf9a6644a04c670
Newnonel <olive_tree132@hotmail.com>
2013-12-29 21:55:21 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
 #Emergence Project V1.0
 #Date Created 29th December 2013
-#No Idea what we are doing
+#No Idea what We are Doing
 
 #Create general program loop
+import time
 Gameover = False;
 
 while not Gameover:
-    Player_Input = input("Press 'e' to exit"); #Gather user Input
+    Player_Input = input("Press 'e' to exit or 'l' to live"); #Gather user Input
     
     #Begin processing user input
     if Player_Input == 'e':
         Gameover = True;
+    elif Player_Input =='l':
+        print("You have choosen to live indeed. A wise choice.")
+        time.wait(10)
